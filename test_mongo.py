17afd18a768db9660fb94a070bfd0fe0ff83648d
lkj01010 <lkj01010@163.com>
2015-08-22 02:24:11 +0800

    test pycharm git

--- test_mongo.py
+++ test_mongo.py
@@ -1,6 +1,30 @@
 import pymongo
+import random
 
-conn = pymongo.MongoClient("10.211.55.6", 27017)
+class record(object):
+    def __init__(self):
+        pass
 
-print conn
+    def run(self):
+        self.conn = pymongo.MongoClient("10.211.55.6", 27017)
+        pass
 
+    def add_batch_record(self):
+        r1 = random.uniform(1, 100)
+        r2 = random.uniform(1, 100)
+        r3 = random.uniform(1, 100)
+        r4 = random.uniform(1, 100)
+        for idx in range(1, 1000):
+            str = {'k1':r1, 'k2':r2, 'k3':r3, 'k4':r4}
+            self.conn.insert_one(str)
+
+
+    def remove_record_all(self):
+        self.conn.remove
+
+    def add_record(self):
+        pass
+
+
+if __name__ == "__main__":
+    r = record()
