a13134eda68f303e25764a931a81d877ce705098
mid <86190827@qq.com>
2015-10-19 19:02:03 +0800

    update console

--- gm/gmserver.py
+++ gm/gmserver.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import os.path
 import random
 
@@ -6,9 +7,33 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import pymongo
+from log import server_log
+
 from tornado.options import define, options
+
+define("db_addr", default="203.195.243.33", help="db addr", type=str)
+define("db_port", default=27017, help="db port", type=int)
 define("port", default=8012, help="run on the given port", type=int)
 
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [(r'/', IndexHandler),
+                    (r'/login', LoginHandler),
+                    (r'/userDetail', UserDetailHandler)]
+
+        server_log.info('gm server start on db[' + options.db_addr + ':' + str(options.db_port) + ']')
+
+        conn = pymongo.MongoClient(options.db_addr, options.db_port)
+        self.db = conn['dota']
+
+        tornado.web.Application.__init__(self,
+                                        handlers,
+                                        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+                                        static_path=os.path.join(os.path.dirname(__file__), "static"),
+                                        ui_modules={'UserData': UserDataModule},
+                                        debug=True)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -18,28 +43,41 @@ class LoginHandler(tornado.web.RequestHandler):
     def post(self):
         self.render('user_query.html')
 
-class PoemPageHandler(tornado.web.RequestHandler):
+class UserDetailHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index_login.html')
 
     def post(self):
-        noun1 = self.get_argument('noun1')
-        noun2 = self.get_argument('noun2')
-        verb = self.get_argument('verb')
-        noun3 = self.get_argument('noun3')
-        self.render('poem.html', roads=noun1, wood=noun2, made=verb,
-                difference=noun3)
+        userno = self.get_argument('userno')
+        user_data = self.application.db.user.find_one({'record.userno': userno})
+        ret_msg = str()
+        if not user_data or len(user_data) == 0:
+           ret_msg = u"没有该用户"
+        else:
+           ret_msg = u"用户编号：" + userno
+        if not user_data:
+            user_data = dict()
+        self.render('user_query_detail.html', ret_msg=ret_msg, data=user_data)
+
+class UserDataModule(tornado.web.UIModule):
+    def render(self, data):
+        if data and 'record' in data.keys():
+            return self.render_string('module/user_data.html', items=data['record'])
+        else:
+            return self.render_string('module/user_data.html', items=dict())
+
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = tornado.web.Application(
-        handlers=[(r'/', IndexHandler),
-                  (r'/login', LoginHandler),
-                  (r'/poem', PoemPageHandler)],
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        debug=True
-    )
+    # app = tornado.web.Application(
+    #     handlers=[(r'/', IndexHandler),
+    #               (r'/login', LoginHandler),
+    #               (r'/userDetail', UserDetailHandler)],
+    #     template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    #     static_path=os.path.join(os.path.dirname(__file__), "static"),
+    #     debug=True
+    # )
+    app = Application()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
